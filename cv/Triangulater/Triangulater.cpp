a961eb0f3809821963affdab7827740b0394a285
lancelot <3128243880@qq.com>
2017-03-24 19:25:39 +0800

    triangluater err

--- cv/Triangulater/Triangulater.cpp
+++ cv/Triangulater/Triangulater.cpp
@@ -1,3 +1,4 @@
+#include <ceres/ceres.h>
 #include "Triangulater.h"
 
 #include "../DataStructure/viFrame.h"
@@ -5,10 +6,66 @@
 #include "../DataStructure/cv/Feature.h"
 #include "../DataStructure/cv/Point.h"
 
+
+class depthErr : public ceres::SizedCostFunction<1, 1> {
+public:
+    virtual ~depthErr() {}
+    depthErr(std::shared_ptr<viFrame> nextFrame, double I_k,
+             const Sophus::SE3d &T_nk, const cvMeasure::features_t::value_type &f)
+            : T_nk_(nextFrame->getT_BS().inverse() * T_nk * nextFrame->getT_BS()), f_(f) {
+        this->nextFrame = nextFrame;
+        this->I_k = I_k;
+    }
+
+    virtual bool Evaluate(double const* const* parameters,
+                          double* residuals,
+                          double** jacobians) const {
+        double d = **parameters;
+        Eigen::Vector3d P_next = T_nk_ * (d * f_->f);
+        auto &cam = nextFrame->getCam();
+        auto uv = cam->world2cam(P_next);
+        for(int i = 0; i < f_->level; ++i) {
+            uv /= 2.0;
+        }
+        *residuals = nextFrame->getCVFrame()->getIntensityBilinear(uv(0), uv(1), f_->level) - I_k;
+        if (jacobians && jacobians[0]) {
+            Eigen::Vector2d grad = nextFrame->getCVFrame()->getGradBilinear(uv(0), uv(1), f_->level);
+            double Ix, Iy;
+            if (f_->type == Feature::EDGELET) {
+                Eigen::Vector2d &dir = f_->grad;
+                Ix = dir(1) * dir(0);
+                Iy = Ix * grad(0) + dir(1) * dir(1) * grad(1);
+                Ix *= grad(1);
+                Ix += dir(0) * dir(0) * grad(0);
+            } else {
+                Ix = grad(0);
+                Iy = grad(1);
+            }
+
+            Eigen::Matrix<double, 1, 3> Jac;
+            Jac(0, 0) = Ix * cam->fx() / P_next(2);
+            Jac(0, 1) = Iy * cam->fy() / P_next(2);
+            Jac(0, 2) = -Ix * cam->fx()  * P_next(0) / P_next(2) / P_next(2) -
+                        Iy * cam->fy() * P_next(1) / P_next(2) / P_next(2);
+            Jac =  Jac * T_nk_.rotationMatrix();
+            jacobians[0][0] = Jac * f_->f;
+        }
+
+
+    }
+
+private:
+    const Sophus::SE3d T_nk_;
+    const cvMeasure::features_t::value_type f_;
+    std::shared_ptr<viFrame> nextFrame;
+    double I_k;
+};
+
 Triangulater::Triangulater()
 {}
 
-bool Triangulater::triangulate(std::shared_ptr<viFrame> &keyFrame, std::shared_ptr<viFrame> &nextFrame, Sophus::SE3d T_nk)
+bool Triangulater::triangulate(std::shared_ptr<viFrame> &keyFrame,
+                               std::shared_ptr<viFrame> &nextFrame, const Sophus::SE3d &T_nk)
 {
     typedef Eigen::Vector3d     PointType;
 
