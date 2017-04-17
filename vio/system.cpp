3f9fb929ed7c493c1473fd2de1a14e1fe7d3ddc2
lancelot <3128243880@qq.com>
2017-04-17 09:18:57 +0800

    system going

--- vio/system.cpp
+++ vio/system.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "system.h"
+#include "IMU/IMU.h"
+#include "IO/imu/IMUIO.h"
+#include "IO/camera/CameraIO.h"
+#include "IO/image/ImageIO.h"
+#include "IMU/IMU.h"
+#include "DataStructure/cv/Camera"
+#include "DataStructure/cv/Feature.h"
+#include "DataStructure/cv/cvFrame.h"
+#include "DataStructure/viFrame.h"
+#include "DataStructure/imu/imuFactor.h"
+#include "cv/FeatureDetector"
+#include "cv/Triangulater"
+#include "cv/Tracker"
+
+system::system(std::string &imuDatafile, std::string &imuParamfile, std::string &camDatafile, std::string &camParamfile,
+               std::string &imageFile, std::string &dataDirectory) {
+	std::shared_ptr<CameraIO> camIO = std::make_pair<CameraIO>(camDatafile, camParamfile);
+}
