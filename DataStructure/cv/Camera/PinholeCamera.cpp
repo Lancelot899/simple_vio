ab264f40e9f1259fea489ff089228166cc2013d1
zzw_linux <1059383105@qq.com>
2017-04-26 21:04:59 +0800

    add pyramid in camera parameter

--- DataStructure/cv/Camera/PinholeCamera.cpp
+++ DataStructure/cv/Camera/PinholeCamera.cpp
@@ -11,28 +11,46 @@ PinholeCamera::PinholeCamera(double width, double height,
                              double fx, double fy,
                              double cx, double cy,
                              double d0, double d1, double d2, double d3, double d4) :
-        AbstractCamera(width, height),
-        fx_(fx), fy_(fy), cx_(cx), cy_(cy),
+        AbstractCamera(width, height),        
         distortion_(false),
         undist_map1_(height_, width_, CV_16SC2),
         undist_map2_(height_, width_, CV_16SC2),
         use_optimization_(false) {
     d_[0] = d0; d_[1] = d1; d_[2] = d2; d_[3] = d3; d_[4] = d4;
-    cvK_ = (cv::Mat_<double>(3, 3) << fx_, 0.0, cx_, 0.0, fy_, cy_, 0.0, 0.0, 1.0);
+
+    fx_ = new double[IMG_LEVEL]; fy_ = new double[IMG_LEVEL];
+    cx_ = new double[IMG_LEVEL]; cy_ = new double[IMG_LEVEL];
+
+    fx_[0] = fx, fy_[0] = fy, cx_[0] = cx, cy_[0] = cy;
+    cvK_[0] = (cv::Mat_<double>(3, 3) << fx_[0], 0.0, cx_[0], 0.0, fy_[0], cy_[0], 0.0, 0.0, 1.0);
     cvD_ = (cv::Mat_<double>(1, 5) << d_[0], d_[1], d_[2], d_[3], d_[4]);
-    cv::initUndistortRectifyMap(cvK_, cvD_, cv::Mat_<double>::eye(3,3), cvK_,
+
+    for (int i = 1; i < IMG_LEVEL; ++i) {
+        fx_[i]  = fx[i-1]/2; fy_[i] = fy[i-1]/2;
+        cx_[i]  = cx[i-1]/2; cy_[i] = cy[i-1]/2;
+        cvK_[i] = (cv::Mat_<double>(3, 3) << fx_[i], 0.0, cx_[i], 0.0, fy_[i], cy_[i], 0.0, 0.0, 1.0);
+    }
+
+
+    cv::initUndistortRectifyMap(cvK_[0], cvD_, cv::Mat_<double>::eye(3,3), cvK_[0],
                                 cv::Size(width_, height_), CV_16SC2, undist_map1_, undist_map2_);
-    K_ << fx_, 0.0, cx_, 0.0, fy_, cy_, 0.0, 0.0, 1.0;
-    K_inv_ = K_.inverse();
+    for (int var = 0; var < IMG_LEVEL; ++var) {
+        K_[var] << fx_[var], 0.0, cx_[var], 0.0, fy_[var], cy_[var], 0.0, 0.0, 1.0;
+        K_inv_[var] = K_[var].inverse();
+    }
+
 }
 
-PinholeCamera::~PinholeCamera() {}
+PinholeCamera::~PinholeCamera() {
+    delete []fx_; delete []fy_;
+    delete []cx_; delete []cy_;
+}
 
 Vector3d PinholeCamera::cam2world(const double& u, const double& v) const {
     Vector3d xyz;
     if(!distortion_) {
-        xyz[0] = (u - cx_)/fx_;
-        xyz[1] = (v - cy_)/fy_;
+        xyz[0] = (u - cx_[0])/fx_[0];
+        xyz[1] = (v - cy_[0])/fy_[0];
         xyz[2] = 1.0;
     }
 
@@ -40,7 +58,7 @@ Vector3d PinholeCamera::cam2world(const double& u, const double& v) const {
         cv::Point2d uv(u,v), px;
         const cv::Mat src_pt(1, 1, CV_32FC2, &uv.x);
         cv::Mat dst_pt(1, 1, CV_32FC2, &px.x);
-        cv::undistortPoints(src_pt, dst_pt, cvK_, cvD_);
+        cv::undistortPoints(src_pt, dst_pt, cvK_[0], cvD_);
         xyz[0] = px.x;
         xyz[1] = px.y;
         xyz[2] = 1.0;
@@ -62,8 +80,8 @@ Vector2d PinholeCamera::world2cam(const Vector3d& xyz) const {
 Vector2d PinholeCamera::world2cam(const Vector2d& uv) const {
     Vector2d px;
     if(!distortion_) {
-        px[0] = fx_*uv[0] + cx_;
-        px[1] = fy_*uv[1] + cy_;
+        px[0] = fx_[0]*uv[0] + cx_[0];
+        px[1] = fy_[0]*uv[1] + cy_[0];
     }
 
     else {
@@ -79,8 +97,8 @@ Vector2d PinholeCamera::world2cam(const Vector2d& uv) const {
         cdist = 1 + d_[0]*r2 + d_[1]*r4 + d_[4]*r6;
         xd = x*cdist + d_[2]*a1 + d_[3]*a2;
         yd = y*cdist + d_[2]*a3 + d_[3]*a1;
-        px[0] = xd*fx_ + cx_;
-        px[1] = yd*fy_ + cy_;
+        px[0] = xd*fx_[0] + cx_[0];
+        px[1] = yd*fy_[0] + cy_[0];
     }
     return px;
 }
@@ -88,8 +106,8 @@ Vector2d PinholeCamera::world2cam(const Vector2d& uv) const {
 Vector2d PinholeCamera::world2camUV(const Vector2d& uv) const {
     Vector2d px;
     if(!distortion_) {
-        px[0] = fx_*uv[0] + cx_;
-        px[1] = fy_*uv[1] + cy_;
+        px[0] = fx_[0]*uv[0] + cx_[0];
+        px[1] = fy_[0]*uv[1] + cy_[0];
     }
 
     else {
@@ -105,8 +123,8 @@ Vector2d PinholeCamera::world2camUV(const Vector2d& uv) const {
         cdist = 1 + d_[0]*r2 + d_[1]*r4 + d_[4]*r6;
         xd = x*cdist + d_[2]*a1 + d_[3]*a2;
         yd = y*cdist + d_[2]*a3 + d_[3]*a1;
-        px[0] = xd*fx_ + cx_;
-        px[1] = yd*fy_ + cy_;
+        px[0] = xd*fx_[0] + cx_[0];
+        px[1] = yd*fy_[0] + cy_[0];
     }
     return px;
 }
